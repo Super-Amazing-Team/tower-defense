41896d5fb9538697475f5c2b1d2d0223827afa03
ax52 <ax51@bk.ru>
2023-02-03 13:24:59 +0300

    chore: added @/ and #root/ base paths

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "#root": resolve(__dirname),
+    },
+  },
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
@@ -12,4 +19,4 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
-})
+});
