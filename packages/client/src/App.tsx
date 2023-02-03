26c4a7330236e53e1815846f94d6e70d7e9eeb7f
ax52 <ax51@bk.ru>
2023-02-03 13:57:27 +0300

    feat: add base ui for counting bears

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,53 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect } from "react";
+import CSSBaseLine from "@mui/material/CssBaseline";
+import { Box, Grid, Typography, Button } from "@mui/material";
+
+import { useBearsStore } from "@/store";
+
+function DummyTestComponent() {
+  const bears = useBearsStore((store) => store.bears);
+  const addOne = useBearsStore((store) => store.addOne);
+  const resetPopulation = useBearsStore((store) => store.resetPopulation);
+
+  return (
+    <Box sx={{ width: "100vw", height: "100vh" }}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        sx={{ height: "100%" }}
+      >
+        <Typography>Вот сколько медведей мы сосчитали: {bears}</Typography>
+        <Box>
+          <Button variant="contained" onClick={addOne}>
+            Сосчитать еще одного
+          </Button>
+          <Button color="error" onClick={resetPopulation}>
+            Начать заново
+          </Button>
+        </Box>
+      </Grid>
+    </Box>
+  );
+}
 
 function App() {
   useEffect(() => {
     const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+      const url = `http://localhost:${__SERVER_PORT__}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    };
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+    fetchServerData();
+  }, []);
+  return (
+    <CSSBaseLine>
+      <DummyTestComponent />
+    </CSSBaseLine>
+  );
 }
 
-export default App
+export default App;
