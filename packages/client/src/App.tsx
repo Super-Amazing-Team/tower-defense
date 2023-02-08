cd30f750f3d63a1c86e3bade3a3e4264a2df1e51
Pavel Glazkov <glazprom@mail.ru>
2023-02-08 09:53:42 +0300

    work with Profile component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,12 +3,18 @@ import { Box, createTheme, IconButton, ThemeProvider } from "@mui/material";
 import CSSBaseLine from "@mui/material/CssBaseline";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProtectedRoutes } from "./utils/ProtectedRoutes";
 import { useLocalStorage, Snackbar } from "@/utils";
-// import { Register } from "@/pages/Register";
-// import { Register } from "@/pages/Register";
-// import { Login } from "@/pages/Login";
+import { Login } from "@/pages/Login";
+import { Register } from "@/pages/Register";
+import { Forum } from "@/pages/Forum";
+import { Topic } from "@/pages/Topic";
+import { Game } from "@/pages/Game";
+import { Leaderboard } from "@/pages/Leaderboard";
 import { Profile } from "@/pages/Profile";
-// import { Leaderboard } from "@/pages/Leaderboard";
+import { Page404 } from "@/pages/Page404";
+import { Page500 } from "@/pages/Page500";
 
 function App() {
   const [mode, setMode] = useLocalStorage("mode", "light");
@@ -33,30 +39,48 @@ function App() {
     [mode],
   );
   return (
-    <ThemeProvider theme={theme}>
-      <CSSBaseLine>
-        <Box
-          sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-        >
-          <Box>
-            <IconButton
-              sx={{ mx: 1 }}
-              onClick={colorMode.toggleColorMode}
-              color="inherit"
-            >
-              {theme.palette.mode === "dark" ? (
-                <Brightness7Icon />
-              ) : (
-                <Brightness4Icon />
-              )}
-            </IconButton>
-            {theme.palette.mode} mode
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CSSBaseLine>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              minHeight: "100vh",
+            }}
+          >
+            <Box>
+              <IconButton
+                sx={{ mx: 1 }}
+                onClick={colorMode.toggleColorMode}
+                color="inherit"
+              >
+                {theme.palette.mode === "dark" ? (
+                  <Brightness7Icon />
+                ) : (
+                  <Brightness4Icon />
+                )}
+              </IconButton>
+              {theme.palette.mode} mode
+            </Box>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="*" element={<Page404 />} />
+              <Route path="/page500" element={<Page500 />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/forum" element={<Forum />} />
+                <Route path="/forum/:id" element={<Topic />} />
+              </Route>
+            </Routes>
           </Box>
-          <Profile />
-        </Box>
-        <Snackbar />
-      </CSSBaseLine>
-    </ThemeProvider>
+          <Snackbar />
+        </CSSBaseLine>
+      </ThemeProvider>
+    </Router>
   );
 }
 
--- packages/client/src/pages/Profile/Profile.tsx
+++ packages/client/src/pages/Profile/Profile.tsx
@@ -1,105 +1,19 @@
-import { Avatar, Button, ButtonGroup, Container, Dialog } from "@mui/material";
-import React from "react";
-import FormProfile from "@/pages/Profile/FormProfile";
-import { useProfileStore } from "@/store";
-import EditPasswordForm from "@/pages/Profile/EditPasswordForm";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "@/store";
 
 export function Profile() {
-  const user = useProfileStore((store) => store.user);
-  const isEditModeState = useProfileStore((store) => store.isEditMode);
-  const setIsEditMode = useProfileStore((store) => store.updateEditMode);
-  const [openEditPasswordModal, setOpenEditPasswordModal] =
-    React.useState(false);
+  const logout = useUserStore((store) => store.logout);
+  const navigate = useNavigate();
 
-  const handleOpenModalEditPassword = () => {
-    setOpenEditPasswordModal(true);
+  const logoutMe = (e: { preventDefault: () => void }) => {
+    logout();
+    navigate("/");
   };
-  const handleCloseEditPasswordModal = () => {
-    setOpenEditPasswordModal(false);
-  };
-  const handleEditMode = () => {
-    setIsEditMode(true);
-  };
-
-  const buttons = [
-    <Button key="one" onClick={handleEditMode}>
-      Редактировать
-    </Button>,
-    <Button key="two" onClick={handleOpenModalEditPassword}>
-      Изменить пароль
-    </Button>,
-    <Button key="three">Выход</Button>,
-  ];
-
-  function handleCloseModalEditAvatar() {
-    console.log("handleCloseModalEditAvatar");
-  }
-
-  function onChangeInputFile(event: React.ChangeEvent<HTMLInputElement>) {
-    console.log(`Saving ${event.target.value}`);
-  }
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Avatar
-        alt={user.login}
-        src={user.avatar}
-        sx={{ width: 64, height: 64, m: "0 auto" }}
-      />
-      <Button
-        component="label"
-        size="small"
-        variant="contained"
-        color="primary"
-        sx={{
-          width: "180px",
-          height: "28px",
-          m: "20px auto",
-        }}
-      >
-        ЗАГРУЗИТЬ ФОТО
-        <input
-          onChange={onChangeInputFile}
-          hidden
-          accept="image/*"
-          multiple
-          type="file"
-        />
-      </Button>
-      <FormProfile isEditMode={isEditModeState} user={user} />
-
-      {!isEditModeState && (
-        <ButtonGroup
-          sx={{
-            width: 164,
-            m: "0 auto",
-          }}
-          orientation="vertical"
-          aria-label="vertical contained button group"
-          variant="text"
-        >
-          {buttons}
-        </ButtonGroup>
-      )}
-
-      <Dialog
-        open={openEditPasswordModal}
-        onClose={handleCloseEditPasswordModal}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <EditPasswordForm
-          onCloseModalEditPassword={handleCloseEditPasswordModal}
-        />
-      </Dialog>
-    </Container>
+    <div>
+      <h1>I&apos;m Profile Component</h1>
+      <button onClick={logoutMe}>Log out</button>
+    </div>
   );
 }
