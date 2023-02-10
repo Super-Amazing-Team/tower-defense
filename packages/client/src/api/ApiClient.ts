993e8ecebe2a2be29c68c9d8bcf5d8ea3aebb8f2
Evgeny Danilchenko <jodan@aaanet.ru>
2023-02-10 19:06:59 +0300

    Update packages/client/src/api/ApiClient.ts
    Co-authored-by: Alexander <117439163+Ax52@users.noreply.github.com>

--- packages/client/src/api/ApiClient.ts
+++ packages/client/src/api/ApiClient.ts
@@ -1,21 +1,7 @@
 import ApiServices from "./baseService";
 import type { IApiService } from "./baseService";
 
-class ApiClient {
-  APIService: IApiService;
+const getAllLeaderboard = (body: { ratingFieldName: string; cursor: number; limit: number }) =>
+  APIService.post('/leaderboard/all', body)
 
-  constructor() {
-    this.APIService = ApiServices;
-  }
-
-  /** @description POST   /leaderboard/all   Get all leaderboard */
-  getAllLeaderboard(body: {
-    ratingFieldName: string;
-    cursor: number;
-    limit: number;
-  }) {
-    return this.APIService.post("/leaderboard/all", body);
-  }
-}
-
-export default new ApiClient();
+export { getAllLeaderboard }
