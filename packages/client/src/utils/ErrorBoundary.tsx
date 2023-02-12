f71942e3f1c64cfedaa51e439d4f730d847f2841
itwassoclear <itwassoclear@gmail.com>
2023-02-12 10:44:39 +0100

    feat: add styles for error component

--- packages/client/src/utils/ErrorBoundary.tsx
+++ packages/client/src/utils/ErrorBoundary.tsx
@@ -3,14 +3,45 @@ import {
   ErrorBoundary as ReactErrorBoundary,
   FallbackProps,
 } from "react-error-boundary";
+import { Container, Typography, Button, Grid } from "@mui/material";
 
 function errorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error?.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+    <Container
+      maxWidth="xs"
+      sx={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        mt: 20,
+      }}
+    >
+      <Typography
+        variant="h5"
+        mb={3}
+        sx={{ textTransform: "uppercase", textAlign: "center" }}
+      >
+        Something went wrong
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h5" mb={3}>
+            {error?.message}
+          </Typography>
+          <Button
+            disableElevation
+            fullWidth
+            size="large"
+            variant="contained"
+            color="primary"
+            onClick={resetErrorBoundary}
+          >
+            Try again
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
