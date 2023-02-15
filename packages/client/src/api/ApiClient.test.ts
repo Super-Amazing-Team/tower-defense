6996bc42fffb3793568d09263c522de2ec14e939
Евгений Данильченко <evgeniy.danilchenko@osinit.com>
2023-02-15 19:52:05 +0300

    add ApiClient.test

---
+++ packages/client/src/api/ApiClient.test.ts
@@ -0,0 +1,63 @@
+import type { AxiosError } from "axios";
+import { signUp, signIn, getAllLeaderboard } from "./ApiClient";
+
+describe("ApiClient", () => {
+  test("should return data signUp", async () => {
+    const body = {
+      email: "test",
+      first_name: "test",
+      login: "test",
+      password: "12345678",
+      phone: "77777777777",
+      second_name: "test",
+    };
+    let err;
+    try {
+      await signUp(body);
+    } catch (error) {
+      const { response } = error as AxiosError<{ reason: string }>;
+      if (response?.data.reason) {
+        err = response.data.reason;
+      }
+    } finally {
+      expect(err).toEqual("email is not valid");
+    }
+  });
+
+  test("should return data signIn", async () => {
+    const body = {
+      login: "login",
+      password: "password",
+    };
+    let err;
+    try {
+      await signIn(body);
+    } catch (error) {
+      const { response } = error as AxiosError<{ reason: string }>;
+      if (response?.data) {
+        err = response.data;
+      }
+    } finally {
+      expect(err).toEqual("Internal Server Error");
+    }
+  });
+
+  test("should return data getAllLeaderboard", async () => {
+    const body = {
+      ratingFieldName: "rating",
+      cursor: 0,
+      limit: 10,
+    };
+    let err;
+    try {
+      await getAllLeaderboard(body);
+    } catch (error) {
+      const { response } = error as AxiosError<{ reason: string }>;
+      if (response?.data.reason) {
+        err = response.data.reason;
+      }
+    } finally {
+      expect(err).toEqual("Cookie is not valid");
+    }
+  });
+});
