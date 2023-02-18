7a310c59f7f36342bc42ebd8918bacd73ce04b72
ax52 <ax51@bk.ru>
2023-02-19 01:21:11 +0300

    feat: added new tests & fix AliClient call

--- packages/client/src/api/ApiClient.test.ts
+++ packages/client/src/api/ApiClient.test.ts
@@ -1,19 +1,51 @@
 import type { AxiosError } from "axios";
-import { signUp, signIn, getAllLeaderboard } from "./ApiClient";
+import { ApiClient } from "./ApiClient";
+import type { TNullable } from "@/utils";
 
-describe("ApiClient", () => {
-  test("should return data signUp", async () => {
-    const body = {
+const { signUp, signIn, getAllLeaderboard } = ApiClient;
+
+const MOCK = {
+  valid: {
+    leaderboard: {
+      ratingFieldName: "rating",
+      cursor: 0,
+      limit: 10,
+    },
+    register: {
+      first_name: "string",
+      second_name: "string",
+      login: `string${Math.round(Math.random() * 10_000)}`,
+      email: `string${Math.round(Math.random() * 10_000)}@asd.asd`,
+      password: "stringString123",
+      phone: "0123456789",
+    },
+    get updateProfile() {
+      return { ...this.register, display_name: this.register.first_name };
+    },
+  },
+  invalid: {
+    login: {
+      login: "login",
+      password: "password",
+    },
+    register: {
       email: "test",
       first_name: "test",
       login: "test",
       password: "12345678",
       phone: "77777777777",
       second_name: "test",
-    };
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  cache: {} as Record<string, any>,
+};
+
+describe("ApiClient", () => {
+  test("should not register with invalid email", async () => {
     let err;
     try {
-      await signUp(body);
+      await signUp(MOCK.invalid.register);
     } catch (error) {
       const { response } = error as AxiosError<{ reason: string }>;
       if (response?.data.reason) {
@@ -24,14 +56,10 @@ describe("ApiClient", () => {
     }
   });
 
-  test("should return data signIn", async () => {
-    const body = {
-      login: "login",
-      password: "password",
-    };
+  test("should throw internal server error", async () => {
     let err;
     try {
-      await signIn(body);
+      await signIn(MOCK.invalid.login);
     } catch (error) {
       const { response } = error as AxiosError<{ reason: string }>;
       if (response?.data) {
@@ -42,15 +70,10 @@ describe("ApiClient", () => {
     }
   });
 
-  test("should return data getAllLeaderboard", async () => {
-    const body = {
-      ratingFieldName: "rating",
-      cursor: 0,
-      limit: 10,
-    };
+  test("should reject without cookie", async () => {
     let err;
     try {
-      await getAllLeaderboard(body);
+      await getAllLeaderboard(MOCK.valid.leaderboard);
     } catch (error) {
       const { response } = error as AxiosError<{ reason: string }>;
       if (response?.data.reason) {
@@ -59,5 +82,64 @@ describe("ApiClient", () => {
     } finally {
       expect(err).toEqual("Cookie is not valid");
     }
+  });
+
+  describe("Work with registered user", () => {
+    test("should register random user", async () => {
+      type TMock = TNullable<Awaited<ReturnType<typeof ApiClient.signUp>>>;
+      const mockUser = MOCK.valid.register;
+      let attempts = 3;
+      let result: TMock = null;
+      async function attempt() {
+        try {
+          if (!attempts) return;
+          result = await ApiClient.signUp(mockUser);
+        } catch {
+          attempts--;
+          attempt();
+        }
+      }
+      await attempt();
+      if (result && typeof result === "object" && "id" in result) {
+        MOCK.cache.user = {
+          login: mockUser.login,
+          password: mockUser.password,
+        };
+      }
+
+      expect(typeof (result as TMock)?.id).toBe("number");
+    });
+
+    test("should logout after register", async () => {
+      let res;
+      try {
+        res = await ApiClient.logout();
+      } catch {
+        res = false;
+      }
+      expect(res).toBeTruthy();
+    });
+
+    test("should login registered user", async () => {
+      const a = MOCK.cache.user;
+
+      let res;
+      try {
+        res = await ApiClient.signIn(a);
+      } catch {
+        res = false;
+      }
+      expect(res).toBeTruthy();
+    });
+
+    test("should change profile", async () => {
+      let res;
+      try {
+        res = await ApiClient.changeUserProfile(MOCK.valid.updateProfile);
+      } catch (err) {
+        res = false;
+      }
+      expect(res).toBeTruthy();
+    });
   });
 });
