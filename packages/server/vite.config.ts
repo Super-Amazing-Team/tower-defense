30f6932bdfd62aab03e59b526962ce9960d17699
Евгений Данильченко <evgeniy.danilchenko@osinit.com>
2023-03-28 22:31:43 +0300

    fix

--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -1,18 +1,16 @@
 /* eslint-disable */
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import eslintPlugin from "vite-plugin-eslint";
+import path from "path";
+// import react from "@vitejs/plugin-react";
+// import tsconfigPaths from "vite-tsconfig-paths";
+// import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react(), eslintPlugin(), tsconfigPaths()],
-  server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
-  },
-  define: {
-    __SERVER_PORT__: process.env.SERVER_PORT || 3001,
-  },
-  build: {
-    minify: false,
-  },
+  resolve:{
+    alias: [
+      {
+        find: '@', replacement: path.resolve(__dirname, '../client/src')
+      }
+    ]
+  }
 });
