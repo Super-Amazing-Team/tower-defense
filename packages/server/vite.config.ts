a72d727b1b3732abf4bacf79a962a031fbea8a13
Евгений Данильченко <evgeniy.danilchenko@osinit.com>
2023-03-28 23:24:17 +0300

    fix error dev server

--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -1,11 +1,16 @@
 /* eslint-disable */
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 import path from "path";
 // import react from "@vitejs/plugin-react";
 // import tsconfigPaths from "vite-tsconfig-paths";
 // import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    minify: false,
+  },
   resolve:{
     alias: [
       {
