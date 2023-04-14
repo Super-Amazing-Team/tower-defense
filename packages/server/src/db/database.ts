ccbf1debbeae0ebfb84ecc2a6824db9841456eb7
jodan13 <jodan@aaanet.ru>
2023-04-14 22:01:54 +0300

    fix server port

--- packages/server/src/db/database.ts
+++ packages/server/src/db/database.ts
@@ -1,12 +1,15 @@
 import type {SequelizeOptions} from "sequelize-typescript";
 import { Sequelize } from "sequelize"
 
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
+  process.env;
+
 export const sequelizeOptions: SequelizeOptions = {
     host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
+    port: Number(POSTGRES_PORT),
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DB,
     dialect: "postgres", // 'mysql', 'sqlite', 'mariadb', 'mssql'
 };
 
