d023b6a5ab586830bf75c15cbd3eef3f458515a5
jodan13 <jodan@aaanet.ru>
2023-04-26 15:58:05 +0300

    fix ssl

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -2,4 +2,4 @@ export const baseUrl = "https://ya-praktikum.tech/api/v2";
 export const REDIRECT_URI =
   "https://super-amazing-team-tower-defense-22.ru/login";
 export const OAUTH_URI = "https://oauth.yandex.ru/authorize?response_type=code";
-export const forumUrl = "https://super-amazing-team-tower-defense-22.ru:5173/";
+export const forumUrl = "http://super-amazing-team-tower-defense-22.ru:5173/";
