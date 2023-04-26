916053454e218481060287250bc813bd34fbc974
noone <kolosochek@gmail.com>
2023-04-26 09:58:14 +0200

    font fix

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -54,7 +54,6 @@ export const gameTheme = createTheme({
           font-display: swap;
           font-weight: 400;
           src: local("PressStart2P"), local("PressStart2P-Regular"), url(${PressStart2P}) format("woff2");
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
       `,
     },
